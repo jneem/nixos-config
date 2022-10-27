bdc88516974f8148b36e6e658b814310f7982498
Joe Neeman <joeneeman@gmail.com>
2022-10-27 17:13:59 -0500

    Add root ssh key.

--- users/jneeman/common.nix
+++ users/jneeman/common.nix
@@ -12,6 +12,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFISxQwDN1H8cA9DDRUTQb9YgsY2AuyvjkXDDOoJLeU3 jneeman@mercury"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGpAveBRfqrg7a41+qdOxw5WT3CbEi7dwlgKObSM85YP jneeman@zeus"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF42/GoFYh49Y4/xP1rSzqKjFYQ+vhiFSJkC0RrbpBqd root@mercury"
       ];
     };
 
