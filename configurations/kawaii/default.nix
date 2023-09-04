09b7229d83e63cca20ce61f84ed82ade83a3dce7
Joe Neeman <joeneeman@gmail.com>
2023-09-03 22:45:33 -0500

    Updates for kawaii

--- configurations/kawaii/default.nix
+++ configurations/kawaii/default.nix
@@ -14,5 +14,9 @@
 
   config = {
     greeter.gdm.enable = true;
+
+    users.users.root.openssh.authorizedKeys.keys = [
+      ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGpAveBRfqrg7a41+qdOxw5WT3CbEi7dwlgKObSM85YP'' 
+    ];
   };
 }
